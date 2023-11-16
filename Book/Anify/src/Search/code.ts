c11cfa688de5bc0fc50f44f480d87b3a27be3088
Eltik <76538547+Eltik@users.noreply.github.com>
2023-11-16 10:52:13 -0500

    update: Anify module

--- Book/Anify/src/Search/code.ts
+++ Book/Anify/src/Search/code.ts
@@ -3,13 +3,15 @@ import { Manga } from "../anify-types";
 export {};
 
 async function logic(payload: BasePayload) {
-    const data: Manga[] = JSON.parse(await sendRequest(`https://api.anify.tv/search?query=${encodeURIComponent(payload.query)}&type=manga`, {}));
+    const data: {
+        results: Manga[],
+        total: number,
+        lastPage: number
+    } = JSON.parse(await sendRequest(`https://api.anify.tv/search?query=${encodeURIComponent(payload.query)}&type=manga`, {}));
 
     const titles: SearchData = [];
 
-    for (let i = 0; i < data.length; i++) {
-        const hasSub = true;
-        const hasDub = true;
+    for (let i = 0; i < data.results.length; i++) {
         const currentCount = data[i].chapters?.latest?.latestChapter ?? 0;
         const totalCount = data[i].totalChapters ?? 0;
 
